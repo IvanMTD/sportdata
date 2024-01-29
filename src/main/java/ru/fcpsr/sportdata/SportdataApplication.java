b2e10c99e9fdd34841719e9d4abbd2ce15702118
Ivan Morgan <morgantdarck@gmail.com>
2024-01-29 15:33:50 +0300

    Initial commit

---
+++ src/main/java/ru/fcpsr/sportdata/SportdataApplication.java
@@ -0,0 +1,11 @@
+package ru.fcpsr.sportdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportdataApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SportdataApplication.class, args);
+	}
+}
