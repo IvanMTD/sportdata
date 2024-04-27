e913e715c9019ecae06340c5c6999ad677e34b5d
IvanMTD <morgantdarck@gmail.com>
2024-04-27 23:24:03 +0300

    update 27.04.2024 (dd.MM.yyyy) * Попытка добавить сохарнение данных ...

---
+++ src/main/java/ru/fcpsr/sportdata/dto/MonitorDTO.java
@@ -0,0 +1,11 @@
+package ru.fcpsr.sportdata.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MonitorDTO {
+    private String content;
+    private String additionally;
+}
