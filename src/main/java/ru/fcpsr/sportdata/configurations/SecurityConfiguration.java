e35529f8c5296e363bd02b66d1be2aca0f6aa6dc
Ivan Morgan <morgantdarck@gmail.com>
2024-05-14 10:17:38 +0300

    update 14.05.2024 (dd.MM.yyyy) * fix исправил поиск по соревнованиям. Теперь имещ без учета регистра.

--- src/main/java/ru/fcpsr/sportdata/configurations/SecurityConfiguration.java
+++ src/main/java/ru/fcpsr/sportdata/configurations/SecurityConfiguration.java
@@ -28,7 +28,9 @@ public class SecurityConfiguration {
 
         return http
                 .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler))
-                .authorizeExchange(auth -> auth.pathMatchers("/auth/login","/auth/user/*","/img/**").permitAll().anyExchange().authenticated())
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/auth/login","/auth/user/*","/img/**","/favicon.ico").permitAll()
+                        .anyExchange().authenticated())
                 .formLogin(loginSpec -> loginSpec.loginPage("/auth/login"))
                 .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(handler))
                 .build();
