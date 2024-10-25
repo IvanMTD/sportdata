450c80fccb46178c40dc72cbb3ac166b298145a0
IvanMTD <morgantdarck@gmail.com>
2024-10-25 21:25:40 +0300

    * небольшие испровления по дизайну

--- src/main/java/ru/fcpsr/sportdata/configurations/ApplicationConfiguration.java
+++ src/main/java/ru/fcpsr/sportdata/configurations/ApplicationConfiguration.java
@@ -1,14 +1,23 @@
 package ru.fcpsr.sportdata.configurations;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.session.CookieWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
+import reactor.core.publisher.Mono;
+import ru.fcpsr.sportdata.enums.Role;
+import ru.fcpsr.sportdata.models.SysUser;
+import ru.fcpsr.sportdata.repositories.UserRepository;
 import ru.fcpsr.sportdata.util.NamingUtil;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
@@ -18,5 +27,36 @@ public class ApplicationConfiguration {
         resolver.setCookieName(NamingUtil.getInstance().getSessionName());
         //resolver.setCookieMaxAge(Duration.ofHours(24));
         return resolver;
+    }
+
+    @Bean
+    public CommandLineRunner prepare(UserRepository userRepository, PasswordEncoder encoder){
+        return args -> {
+            log.info("***************** environments start *******************");
+            for(String key : System.getenv().keySet()){
+                String[] p = key.split("\\.");
+                if(p.length > 1) {
+                    log.info("\"{}\"=\"{}\"", key, System.getenv().get(key));
+                }
+            }
+            log.info("****************** environments end ********************");
+            userRepository.findByUsername("admin").flatMap(existingUser -> {
+                log.info("User {} exist.", existingUser.getEmail());
+                return Mono.just(existingUser);
+            }).switchIfEmpty(
+                    Mono.defer(() -> {
+                        log.info("User not found! Create default admin user: {}, password: {}", "admin","Admin_123!");
+                        SysUser user = new SysUser();
+                        user.setUsername("admin");
+                        user.setPassword(encoder.encode("Admin_123!"));
+                        user.setEmail("karachkov_is@fcpsr.ru");
+                        user.setRole(Role.ADMIN);
+                        return userRepository.save(user);
+                    })
+            ).flatMap(u -> {
+                log.info("User in db: [{}]", u);
+                return Mono.empty();
+            }).subscribe();
+        };
     }
 }
